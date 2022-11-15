c5b31557523eecc21aeb0022171e60376e8c3e6a
EC2 Default User <ec2-user@ip-172-31-8-66.ap-northeast-1.compute.internal>
2022-11-15 16:35:38 +0000

    Add routes root_path(/) to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # ルートパス ｢/｣ にアクセスした時､railsのデフォルト画面ではなく､
+  # スケジュール一覧ページを表示するために､
+  # ルート先をpostsコントローラーのindexアクションに指定する
+  get '/', to: 'posts#index'
+  
   resources :posts
 end
