e19f709d96e3964fe6bbddf897d79c7ec0506e4d
EC2 Default User <ec2-user@ip-172-31-8-66.ap-northeast-1.compute.internal>
2022-11-15 00:58:47 +0000

    Add validates in /models/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  
+  #  スケジュールメモは50文字以内に制限
+  validates :memo, length: { maximum: 50 }
 end
